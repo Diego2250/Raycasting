f715f0725df7a60218592046cea443736cbd0329
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2023-11-12 22:17:56 -0600

    3D map

--- src/raycasting.py
+++ src/raycasting.py
@@ -55,8 +55,12 @@ class Raycasting:
             else:
                 depth = depth_hor
 
-            pg.draw.line(self.game.screen, 'yellow', (100 * ox, 100 * oy),
-                         (100 * ox + 100 * depth * cos_a, 100 * oy + 100 * depth * sin_a), 2)
+            depth *= math.cos(self.game.player.angle - ray_angle) # quitar fisheye
+
+            proj_height = SCREEN_DIST / (depth + 0.0001) # 0.0001 para evitar divisiones por 0
+
+            c = [255 / (1 + depth ** 5 * 0.00002)] * 3
+            pg.draw.rect(self.game.screen, c, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
 
             ray_angle += DELTA_ANGLE
 
